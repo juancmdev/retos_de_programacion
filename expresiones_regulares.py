48c76c5bf5327a7034abba1b9c5ead2b69fd6077
juancmdev <juancmorenoj@gmail.com>
2024-04-23 21:42:49 -0500

    validate email y phone

---
+++ expresiones_regulares.py
@@ -0,0 +1,26 @@
+import re
+
+# """
+# Ejercicio
+# """
+# regex = r"[0-9]+"
+
+# def find_numbers(text: str) -> list:
+#     return re.findall(regex, text)
+
+# print(find_numbers("Este es el ejercicio 16 publicado 15/04/2024."))
+
+"""
+Extra
+"""
+
+def validate_email(email: str) -> bool:
+    return bool(re.match(r"[\w.+-]+@[\w]+\.[a-zA-Z]+", email))
+
+print(validate_email("juancmorenoj@gmail.com"))
+
+def validate_phone(phone: str) -> bool:
+    return bool(re.match(r"^[\d]{3,}$", phone))
+
+print(validate_phone("1234"))
+
