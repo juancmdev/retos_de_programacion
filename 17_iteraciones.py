2e3cc5621c78b2d76cb0adadcaeffcce359b3461
juancmdev <juancmorenoj@gmail.com>
2024-06-15 17:39:41 -0500

    Update 17_iteraciones.py

--- 17_iteraciones.py
+++ 17_iteraciones.py
@@ -3,10 +3,6 @@
  * Utilizando tu lenguaje, emplea 3 mecanismos diferentes para imprimir
  * números del 1 al 10 mediante iteración.
  *
- * DIFICULTAD EXTRA (opcional):
- * Escribe el mayor número de mecanismos que posea tu lenguaje
- * para iterar valores. ¿Eres capaz de utilizar 5? ¿Y 10?
- */
 '''
 
 for i in range(0, 10):
@@ -18,3 +14,10 @@ i = 0
 while i <= 10:
     print(i)
     i += 1
+
+'''
+* DIFICULTAD EXTRA (opcional):
+ * Escribe el mayor número de mecanismos que posea tu lenguaje
+ * para iterar valores. ¿Eres capaz de utilizar 5? ¿Y 10?
+ */
+'''
